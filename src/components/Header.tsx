6a8b1b0c903cc84789f730236def19caf9c8b6d9
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-07-12 00:45:40 +0330

    add text to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 import Navbar from "@/components/Navbar";
 
-
-
-
 function Header() {
-    return (
-        <div className='bg-[url("/header.png")] h-[587px] w-full bg-cover'>
-            <Navbar/>
-        </div>
-    )
+  return (
+    <div className='bg-[url("/header.png")] h-[587px] w-full bg-cover'>
+      <Navbar />
+      <h1 className="px-12  py-14 bg-gradient-to-r from-[#3C005A] to-[#01A688] inline-block  text-transparent bg-clip-text text-bold text-[32px]">سفرجو دری به سوی رویایی ترین خاطرات (:</h1>
+      
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,29 +2,32 @@ import React from "react";
 import { HiBell } from "react-icons/hi";
 
 function Navbar() {
-  const navbarData = [{ title: "پیام ها", src: "bell.svg" }, { title: "پشتیبانی",src:"Iconly.svg" }];
+  const navbarData = [
+    { title: "پیام ها", src: "bell.svg" },
+    { title: "پشتیبانی", src: "Iconly.svg" },
+  ];
 
   return (
     <nav className="flex flex-row items-center justify-between px-12 text-lg py-2 font-semibold">
+      <div className="flex flex-row justify-between items-center w-[25%]">
+        <img src="Logo.jpg" alt="" />
+        {navbarData.map((item) => {
+          return (
+            <a
+              href=""
+              className="flex flex-row justify-center items-center mr-2"
+            >
+              <img src={item.src} alt="" className="ml-3" />
+              {item.title}
+            </a>
+          );
+        })}
+      </div>
       <div className="w-[10%]">
         <a href="" className="flex flex-row justify-center items-center">
+          <img src="person.svg" alt="" className="ml-3" />
           ورود / ثبت نام
-          <img src="person.svg" alt="" className="ml-2" />
         </a>
-      </div>
-      <div className="flex flex-row justify-between items-center w-[22%]">
-    {
-        navbarData.map((item)=>{
-            return(
-                <a href="" className="flex flex-row justify-center items-center mr-2">
-                {item.title}
-                <img src={item.src} alt=""  className="ml-2"/>
-              </a>
-            )
-        })
-    }
-       
-        <img src="logo.jpg" alt="" />
       </div>
     </nav>
   );
