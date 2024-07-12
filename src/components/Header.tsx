ddad914a4e4829f9fb0415c8e2f9adfd6d69f3b6
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-07-12 16:57:13 +0330

    Edit logo in Header and create Filter box in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
+import FilterBox from "./FilterBox";
 
-function Header() {
+const Header:React.FC=()=> {
   return (
     <div className='bg-header-image h-[587px] w-full bg-cover'>
       <Navbar />
       <h1 className="px-12  py-14 bg-gradient-to-r from-[#3C005A] to-[#01A688] inline-block  text-transparent bg-clip-text text-bold text-[32px]">سفرجو دری به سوی رویایی ترین خاطرات (:</h1>
-      
+      <FilterBox/>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { HiBell } from "react-icons/hi";
 
-function Navbar() {
+const Navbar:React.FC=()=> {
   const navbarData = [
     { title: "پیام ها", src: "/svg/bell.svg" },
     { title: "پشتیبانی", src: "/svg/Iconly.svg" },
@@ -11,7 +10,7 @@ function Navbar() {
     <nav className="flex flex-row items-center justify-between px-12 text-lg py-2 font-semibold">
       <div className="flex flex-row justify-between items-center w-[25%]">
         <a href="" className="bg-gradient-to-r from-[#7B4794] to-[#01A688]   text-transparent bg-clip-text flex flex-row justify-center items-center text-2xl font-">
-          <img src="/svg/new.png" className="w-10 m-2" alt="" />
+          <img src="/svg/Logo.png" className="w-6 m-2" alt="" />
           سفرجو
         </a>
 
