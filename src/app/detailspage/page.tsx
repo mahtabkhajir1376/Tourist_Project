b63c631cc77324e0922f2f1cc871507f2bd38219
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-07-22 19:28:30 +0330

    create TourOptions in datail page

--- src/app/detailspage/page.tsx
+++ src/app/detailspage/page.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ImageGallarydetailtour from '@/components/ImageGallarydetailtour';
 import Navbar from '@/components/Navbar';
+import TourOptions from '@/components/TourOptions';
 
 function DetailsPage() {
   return (
-    <>
+    <div className='font-iransans'>
     <Navbar/>
     <ImageGallarydetailtour/>
-    </>
+    <div className='flex flex-row w-[90%] mx-auto my-0 h-[985px]'>
+    <TourOptions/>
+    </div>
+    </div>
   )
 }
 
