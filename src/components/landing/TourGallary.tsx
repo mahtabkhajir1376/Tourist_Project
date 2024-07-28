9aa8994058edc44785c3aec09a11a612a876054a
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-07-28 11:38:14 +0330

    responsive Tour gallry in landing page

--- src/components/landing/TourGallary.tsx
+++ src/components/landing/TourGallary.tsx
@@ -28,11 +28,11 @@ const imageData:ImageData =[
 
 const TourGallary:React.FC = () => {
   return (
-    <div className="grid grid-cols-6  w-[90%] h-[800px] mx-auto my-0 gap-5">
-      <div className="col-span-4  grid grid-rows-2 gap-5">
+    <div className="grid grid-cols-6  2xl:w-[90%] 2xl:h-[800px] sm:[80%] mx-auto my-0 2xl:gap-5 sm:gap-3 lg:[80%] lg:gap-2 ">
+      <div className="col-span-4  grid grid-rows-2 2xl:gap-5 sm:gap-2 lg:gap-2">
         <ImageLayout ImageSrc={imageData[1].imageSrc} span="row-span-1" header={imageData[1].header} capacity={imageData[1].Capacity} position={imageData[1].position}/>
 
-        <div className="row-span-1  grid grid-cols-2 gap-5">
+        <div className="row-span-1  grid grid-cols-2 2xl:gap-5 sm:gap-2 lg:gap-2">
           <ImageLayout ImageSrc={imageData[3].imageSrc} span="col-span-1" header={imageData[3].header} capacity={imageData[3].Capacity} position={imageData[3].position} />
 
           <ImageLayout ImageSrc={imageData[2].imageSrc} span="col-span-1" header={imageData[2].header} capacity={imageData[2].Capacity} position={imageData[2].position} />
