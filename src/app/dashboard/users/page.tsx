fa28d86e963ab60f898234d657808b301baaf0be
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-08-06 17:18:48 +0330

    add pagination to users page admin-dashboard

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -4,11 +4,25 @@ import SearchBox from "@/components/dashboard/users/SearchBox";
 
 function page() {
   return (
-    <div className="w-full">
-      <SearchBox />
-      <Users />
+    <div className="w-full flex flex-col justify-between items-center">
+      <div className="w-full ">
+        <SearchBox />
+        <Users />
+      </div>
+      <div className="join font-iransansNumber my-4 w-[25%] flex flex-row justify-between items-center ">
+        <button className=" join-item">
+          <img src="/svg/dashboard/pagination_arrow_right.svg" alt="" />
+        </button>
+        <button className="w-8 h-8 hover:shadow-md hover:shadow-[#E3EBE9] opacity-60 rounded-md hover:opacity-100 ">1</button>
+        <button className="w-8 h-8  hover:shadow-md hover:shadow-[#E3EBE9]  rounded-md opacity-60  hover:opacity-100 ">2</button>
+        <button className="w-8 h-8  hover:shadow-md hover:shadow-[#E3EBE9]  rounded-md opacity-60  hover:opacity-100 ">3</button>
+        <button className="w-8 h-8  hover:shadow-md hover:shadow-[#E3EBE9]  rounded-md opacity-60  hover:opacity-100  ">4</button>
+        <button className="w-8 h-8  hover:shadow-md hover:shadow-[#E3EBE9]  rounded-md opacity-60  hover:opacity-100  ">5</button>
+        <button className="join-item ">
+          <img src="/svg/dashboard/pagination_arrow_left.svg" alt="" />
+        </button>
+      </div>
     </div>
   );
 }
-
 export default page;
--- src/components/dashboard/users/SearchBox.tsx
+++ src/components/dashboard/users/SearchBox.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBox = () => {
   return (
     <div className="flex flex-row justify-between items-center w-[80%] my-4 mx-auto font-iransansNumber" >
-      <label className="input  flex items-center gap-2 w-[41.5%]">
+      <label className="input flex items-center gap-2 w-[41.5%]">
         <input type="text" className="grow placeholder:text-[10px] placeholder:font-thin placeholder:text-[#707070]" placeholder="جستجوی شهر مورد نظر"  />
         <img src="/svg/search.svg" alt="" className="w-4 h-4" />
       </label>
