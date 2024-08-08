5a8aec5c8bd41a76318f95df63e6c4417f3e6eff
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-08-08 20:31:32 +0330

    add marginbottom to filter box in show list page

--- src/components/showlist/PriceRange.tsx
+++ src/components/showlist/PriceRange.tsx
@@ -1,7 +1,14 @@
 "use client";
+import { useState } from "react";
 import React from "react";
 
 const PriceRange: React.FC = () => {
+
+
+  const[pricevalue,setpricevalue]=useState<number>(5000000)
+
+
+  console.log(pricevalue)
   return (
     <div className="w-full mt-16   font-iransansNumber ">
       <h2 className="text-base font-medium mb-5">محدوده قیمت:</h2>
@@ -9,7 +16,7 @@ const PriceRange: React.FC = () => {
         <span className="font-extralight text-sm w-[30%]  mt-4  text-center">
           تا 16.000.000 تومان
         </span>
-        <span className="font-medium text-sm  my-14 text-center">5.000.000 تومان</span>
+        <span className="font-demiBold text-sm  my-14 text-center ">{`${pricevalue}  تومان`}</span>
         <span className="font-extralight text-sm w-[30%] mt-4  text-center">
           از 1.000.000 تومان
         </span>
@@ -21,7 +28,8 @@ const PriceRange: React.FC = () => {
         defaultValue="5000000"
         max="16000000"
         className=" range range-xs  range-success  bg-[#DDFFF8]  h-3  w-full top-[7px] rounded-xl  my-0 mx-auto "
-        defaultChecked
+        value={pricevalue}
+        onChange={(e)=>setpricevalue(Number(e.target.value))}
       />
     </div>
   );
