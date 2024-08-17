3425774bed7a781add1f87ee142876c466851dd5
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-08-17 18:16:35 +0330

    add responsive to header banners page in admin-dashboard

--- src/app/dashboard/banners/page.tsx
+++ src/app/dashboard/banners/page.tsx
@@ -18,7 +18,7 @@ function page() {
   ];
 
   return (
-    <div className="w-full">
+    <div className="w-full relative h-lvh  overflow-y-auto font-iransansNumber">
       <Header />
       <div className=" flex flex-row flex-wrap justify-between items-center w-[88.5%] my-9 mx-auto ">
         {Banner.map((item) => {
@@ -30,9 +30,9 @@ function page() {
         <Button
           bgColor="bg-[#01A657]"
           borderRadius="rounded-md"
-          fontSize="font-medium text-base font-iransansNumber "
+          fontSize="font-medium 2xl:text-sm xl:text-sm md:text-xs lg:text-sm font-iransansNumber "
           titleBtn="ثبت نهایی"
-          width="w-[29%] h-[56px]"
+          width="2xl:w-[16%] xl:w-[16%] md:w-[25%] lg:w-[23%] h-10"
           padding="px-6 py-2"
           textColor="text-white"
           margin="my-11"
