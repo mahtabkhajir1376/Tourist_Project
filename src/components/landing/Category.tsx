6c5a1dec9de84acb8baba0bf920f973db568c19c
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2024-09-01 21:23:27 +0330

    fetch api category in landing page

--- src/components/landing/Category.tsx
+++ src/components/landing/Category.tsx
@@ -1,44 +1,70 @@
+"use client";
 import React from "react";
 import SingleCategory from "./SingleCategory";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const Category: React.FC = () => {
+  type Data = {
+    success: string;
+    message: string;
+    data: item[];
+  };
+  type item = {
+    id: string;
+    title: string;
+    description: string;
+  };
+
   type data = {
     title: String;
     src: string;
-    imageHeight?:string;
+    imageHeight?: string;
   };
 
-  type categoryDate = data[];
+  type categoryDate = string[];
 
-  const categoryDate: categoryDate = [
-    { title: "جنگلی", src: "/image/forest.png" },
-    { title: "ساحلی", src: "/image/beach.png", imageHeight: "h-14" },
-    { title: "زیارتی", src: "/image/mosque.png", imageHeight: "h-14" },
-    { title: "بومگردی", src: "/image/boom.png" },
-    { title: "تاریخی", src: "/image/history.png" },
-    { title: "خارجی", src: "/image/forien.png" },
+  const categoryImages: categoryDate = [
+    "/image/category/forest.png",
+    "/image/category/beach.png",
+    "/image/category/mosque.png",
+    "/image/category/boom.png",
+    "/image/category/history.png",
+    "/image/category/forien.png"
   ];
+
+  const fetchCategory = async (): Promise<Data> => {
+    const response = await axios.get<Data>(
+      "http://mohammad-mokhtari.ir/safarjoo/api/categories"
+    );
+    return response.data;
+  };
+
+  const { data, error, isLoading } = useQuery<Data, Error>({
+    queryKey: ["fetchCategory"],
+    queryFn: fetchCategory,
+  });
+
+  const newArray = data?.data.slice(2, 8);
+const finalCategoryData=newArray?.map((item,index)=>{
+  return{
+    ...item,
+    imageUrl:categoryImages[index]
+  }
+})
+
+console.log(finalCategoryData)
+
+
+
 
   return (
     <div className="flex flex-row justify-around items-center my-20 2xl:w-[83%] mx-auto  xl:w-[95%] sm:w-full lg:w-full sm:justify-around xl:mx-auto ">
-      {categoryDate.map((item) => {
-        return (
-          <SingleCategory
-            title={item.title}
-            imageSrc={item.src}
-            imageHeight={item.imageHeight}
-          />
-        );
+      {finalCategoryData?.map((item) => {
+        return <SingleCategory title={item.title} imageSrc={item.imageUrl} key={item.id} />;
       })}
     </div>
   );
 };
 
 export default Category;
-
-
-
-
-
-
-
